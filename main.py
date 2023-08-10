ad728decbeb2ff4c6bb999e66fe5f2cf8767970f
Daniel Steman <daniel-steman@live.nl>
2023-08-10 15:00:01 +0000

    added get openapi schema

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-from dotenv import load_dotenv
+import json
 import os
+from typing import Dict
+
+import httpx
+from dotenv import load_dotenv
 
 load_dotenv()
 
 api_key = os.environ["API_KEY"]
+
+url = "https://api.schiphol.nl/public-flights/flights"
+
+headers = {
+    "Accept": "application/json",
+    "app_id": "5760df96",
+    "app_key": api_key,
+    "ResourceVersion": "v4",
+}
+
+res = httpx.get(url, headers=headers)
+data = json.loads(res.content)
+
+
+def get_open_api_spec(to_path: str = "data/public-flights-v4.json") -> Dict:
+    openapi_spec_download_link = (
+        "https://developer.schiphol.nl/swagger/spec/public-flights-v4.json"
+    )
+    res = httpx.get(openapi_spec_download_link)
+    content = res.content
+    data = json.loads(content)
+    with open(to_path, "w") as file:
+        json.dump(data, file, indent=4)
+    return data
