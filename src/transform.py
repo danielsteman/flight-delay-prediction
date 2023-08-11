23c53a10d1546ed85a815c15e5a97789198cee69
Daniel Steman <daniel-steman@live.nl>
2023-08-11 13:45:30 +0200

    add flighttransfomer

--- src/transform.py
+++ src/transform.py
@@ -1,7 +1,10 @@
 import json
+import os
 from typing import Dict
 
 import joblib
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 from src.logger import setup_logger
 from src.storage_manager import StorageManager
@@ -10,56 +13,73 @@ from src.utils import filter_nested_dict
 logger = setup_logger()
 
 
-def get_data() -> None:
-    manager = StorageManager("flight-delay-prediction")
-    flights = manager.download_all("raw_flights")
-    manager.save_locally(flights, "raw_flights")
+class FlightTransformer:
+    def __init__(self, bucket: str) -> None:
+        self.storage_manager = StorageManager(bucket)
+        self.get_data()
 
+    def get_data(self) -> None:
+        local_data_path = "data/raw_flights.joblib"
+        if os.path.exists(local_data_path):
+            self.data = joblib.load(local_data_path)
+        else:
+            self.data = self.manager.download_all("raw_flights")
+            self.manager.save_locally(self.data, "raw_flights")
 
-def parse_and_extract(data: str) -> Dict:
-    keys_to_extract = [
-        "expectedTimeGateClosing",
-        "scheduleDateTime",
-        "aircraftType",
-        "codeshares",
-        "flightDirection",
-        "transferPositions",
-        "checkinAllocations",
-        "terminal",
-    ]
-    extracted_data = filter_nested_dict(
-        json.loads(data), keys_to_include=keys_to_extract
-    )
-    return extracted_data
+        logger.info(f"Number of flights in {self.__class__.__name__}: {len(self.data)}")
 
+    @classmethod
+    def parse_and_extract(self, data: str) -> Dict:
+        keys_to_extract = [
+            "expectedTimeGateClosing",
+            "scheduleDateTime",
+            "codeshares",
+            "flightDirection",
+            "transferPositions",
+            "checkinAllocations",
+            "terminal",
+        ]
+        extracted_data = filter_nested_dict(
+            json.loads(data), keys_to_include=keys_to_extract
+        )
+        return extracted_data
 
-def make_tabular(data: Dict) -> Dict:
-    tabular_data = data
-    tabular_data["aircraftType"] = data.get("aircraftType")
-    tabular_data["codeshares"] = (
-        len(data["codeshares"]["codeshares"]) if data["codeshares"] else 0
-    )
-    tabular_data["transferPositions"] = (
-        data["transferPositions"]["transferPositions"][0]
-        if data["transferPositions"]
-        else 0
-    )
-    tabular_data["checkinAllocations"] = (
-        len(data["checkinAllocations"]["checkinAllocations"])
-        if data["checkinAllocations"]
-        else 0
-    )
-    return tabular_data
+    @classmethod
+    def make_tabular(self, data: Dict) -> Dict:
+        tabular_data = data
+        tabular_data["codeshares"] = (
+            len(data["codeshares"]["codeshares"]) if data["codeshares"] else 0
+        )
+        tabular_data["transferPositions"] = (
+            data["transferPositions"]["transferPositions"][0]
+            if data["transferPositions"]
+            else 0
+        )
+        tabular_data["checkinAllocations"] = (
+            len(data["checkinAllocations"]["checkinAllocations"])
+            if data["checkinAllocations"]
+            else 0
+        )
+        return tabular_data
 
+    def transform(self) -> pd.DataFrame:
+        tabular_flights = []
 
-def transform():
-    json_flights = joblib.load("data/raw_flights.joblib")
+        for flight in self.data:
+            dict_flight = self.parse_and_extract(flight)
+            tabular_flight = self.make_tabular(dict_flight)
+            tabular_flights.append(tabular_flight)
 
-    logger.info(f"Number of flights: {len(json_flights)}")
+        df = pd.DataFrame(tabular_flights)
 
-    tabular_flights = []
+        df["expectedTimeGateClosing"] = pd.to_datetime(
+            df["expectedTimeGateClosing"], format="%Y-%m-%dT%H:%M:%S.%f%z"
+        )
+        df["scheduleDateTime"] = pd.to_datetime(
+            df["scheduleDateTime"], format="%Y-%m-%dT%H:%M:%S.%f%z"
+        )
 
-    for flight in json_flights:
-        dict_flight = parse_and_extract(flight)
-        tabular_flight = make_tabular(dict_flight)
-        tabular_flights.append(tabular_flight)
+        label_encoder = LabelEncoder()
+        df["flightDirection"] = label_encoder.fit_transform(df["flightDirection"])
+
+        return df
